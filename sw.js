928ad7ef3843c93f7e69f490de7573f34946a163
Nannys y Peques <nannysypeques@gmail.com>
2026-04-03 12:32:54 -0600

    1

--- sw.js
+++ sw.js
@@ -1,9 +1,18 @@
-const CACHE_NAME = 'ganguitas-cache-v1';
+const CACHE_NAME = 'ganguitas-cache-v2';
 const urlsToCache = [
   './',
   './index.html',
   './style.css',
+  './js_config.js',
+  './js_api.js',
+  './js_data.js',
   './js_main.js',
+  './js_login.js',
+  './js_inventory.js',
+  './js_add_item.js',
+  './js_financials.js',
+  './js_pos.js',
+  './js_scanner.js',
   './Logo-ganguitas.jpg'
 ];
 
@@ -16,7 +25,23 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
+  // Ignorar peticiones POST (API) y enviarlas directo a red
+  if (event.request.method === 'POST') {
+    return;
+  }
+  
   event.respondWith(
     caches.match(event.request)
       .then(response => {
